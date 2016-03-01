8d00a9fafc1ce78af4575d90da734176514547ff
Devan Barger <devanbarger@yahoo.com>
2016-02-29 23:33:27 -0600

    Added two .java programs for next assignment.

--- src/Networks/Program2Client.java
+++ src/Networks/Program2Client.java
@@ -1,7 +1,33 @@
 package Networks;
 
+import java.io.*;
+import java.net.Socket;
+
+
 /**
  * Created by devan on 2/17/2016.
  */
 public class Program2Client {
+    public static void main(String argv[]) throws Exception {
+
+        try{
+            Socket s = new Socket("192.168.254.5", 64386);//server ip and port
+            DataInputStream input = new DataInputStream(s.getInputStream());
+            DataOutputStream output = new DataOutputStream(s.getOutputStream());
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String inputMessage = "";
+            String outputMessage = "";
+
+            while(!inputMessage.equals("EXITEXIT")){
+                outputMessage = br.readLine();
+                output.writeUTF(outputMessage);
+                inputMessage = input.readUTF();
+                System.out.println(inputMessage);
+            }
+
+        }catch(Exception e){
+            //Handle excpetions here
+        }
+    }
 }
--- src/Networks/Program2Server.java
+++ src/Networks/Program2Server.java
@@ -1,7 +1,41 @@
 package Networks;
 
+import javax.xml.bind.SchemaOutputResolver;
+import java.net.*;
+import java.io.*;
+
 /**
  * Created by devan on 2/17/2016.
  */
 public class Program2Server {
+    public static void main(String[] args){
+        try{
+            /**This is the main transport socket for our server*/
+            ServerSocket socket = new ServerSocket(64386);
+
+            /**This is a socket used to accept messages from client.*/
+            Socket sock = socket.accept();
+
+            DataInputStream input = new DataInputStream(sock.getInputStream());
+            DataOutputStream output = new DataOutputStream(sock.getOutputStream());
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String inputMessage = "";
+            String outputMessage = "";
+
+            while (!inputMessage.equals("EXITEXIT")){
+                inputMessage = input.readUTF();
+                System.out.println(inputMessage);
+                outputMessage = br.readLine();
+                output.writeUTF(outputMessage);
+                output.flush();
+            }
+            sock.close();
+
+        }catch(Exception e){
+
+
+        }
+    }
 }
